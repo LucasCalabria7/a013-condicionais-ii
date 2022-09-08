bd5fdef363170c143cd50642d9b39abd1e6c4eaa
Lucas Calabria <lucascalabria86@gmail.com>
2022-09-08 15:37:43 -0300

    Commit condicionais 2

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,25 @@
-const num = //seu numero aqui
+let numero = Number(prompt("Digite seu número aqui"))
 
+if(numero % 2 === 0){
+    if(numero % 3 === 0){
+        console.log("Divisível por 2 e 3")
+    } else{
+        console.log("Somente por 2")
+    }
+}
+else{
+    if(numero % 3 === 0){
+        console.log("Somente por 3")
+    }
+    else{
+        console.log("Nem 2 nem 3")
+    }
+}
+
+
+
+if((numero % 2 === 0) || (numero % 3 === 0)) {
+    console.log("Este número é divisível por 2 ou por 3")
+} else {
+    console.log("Este número não é divisível por 2 ou por 3")
+}
